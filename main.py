0ad83cb838cea8772159245500752027c52ef827
Dan Park <danieljinwoopark@gmail.com>
2017-12-17 17:14:41 -0600

    added functions

--- main.py
+++ main.py
@@ -3,4 +3,39 @@ import datetime
 import pandas as pd
 import matplotlib as plt
 
-print("Hello World")
+request_string = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR"
+response = requests.get(request_string)
+
+def price(symbol, comparison_symbols=['USD'], exchange=''):
+    url = 'https://min-api.cryptocompare.com/data/price?fsym={}&tsyms={}'\
+            .format(symbol.upper(), ','.join(comparison_symbols).upper())
+    if exchange:
+        url += '&e={}'.format(exchange)
+    page = requests.get(url)
+    data = page.json()
+    return data
+
+
+def daily_price_historical(symbol, comparison_symbol, limit=1, aggregate=1, exchange='', allData='true'):
+    url = 'https://min-api.cryptocompare.com/data/histoday?fsym={}&tsym={}&limit={}&aggregate={}&allData={}'\
+            .format(symbol.upper(), comparison_symbol.upper(), limit, aggregate, allData)
+    if exchange:
+        url += '&e={}'.format(exchange)
+    page = requests.get(url)
+    data = page.json()['Data']
+    df = pd.DataFrame(data)
+    df['timestamp'] = [datetime.datetime.fromtimestamp(d) for d in df.time]
+    return df
+
+def minute_price_historical(symbol, comparison_symbol, limit, aggregate, exchange=''):
+    url = 'https://min-api.cryptocompare.com/data/histominute?fsym={}&tsym={}&limit={}&aggregate={}'\
+            .format(symbol.upper(), comparison_symbol.upper(), limit, aggregate)
+    if exchange:
+        url += '&e={}'.format(exchange)
+    page = requests.get(url)
+    data = page.json()['Data']
+    df = pd.DataFrame(data)
+    df['timestamp'] = [datetime.datetime.fromtimestamp(d) for d in df.time]
+    return df
+
+print(price('LTC'))
